c8578e98f96de819ec9c358cdcdaad4198dd9a0e
shadow-light <noreply@gracious.tech>
2022-09-14 17:50:27 +1000

    sw

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 import plugin_vue from '@vitejs/plugin-vue'
 import plugin_vuetify from 'vite-plugin-vuetify'
 import plugin_svg_loader from 'vite-svg-loader'
+import {generateSW} from 'rollup-plugin-workbox'
 import {defineConfig} from 'vite'
 
 import plugin_index from './vite_plugin_index'
@@ -17,6 +18,24 @@ export default defineConfig(({mode}) => {
             plugin_vue(),
             plugin_svg_loader(),
             plugin_vuetify(),
+            generateSW({
+                swDest: 'dist/sw.js',  // WARN Must be at root level to control all paths
+                globDirectory: 'dist',
+                globPatterns: ['**/*'],  // Override default which excludes images etc
+                cacheId: 'fetch',  // Will get -precache-... appended
+                runtimeCaching: [
+                    {
+                        // Runtime caching for any external content (i.e. collections)
+                        // TODO Check periodically rather than every time?
+                        // See https://github.com/GoogleChrome/workbox/issues/3069
+                        handler: 'StaleWhileRevalidate',
+                        urlPattern: route => !route.sameOrigin,
+                        options: {
+                            cacheName: 'fetch-collections',
+                        },
+                    },
+                ],
+            }),
         ],
         resolve: {
             alias: [
@@ -25,6 +44,9 @@ export default defineConfig(({mode}) => {
                     replacement: path.resolve(__dirname, 'src'),
                 },
             ],
+        },
+        css: {
+            devSourcemap: true,  // Include source map when injecting CSS in JS
         },
         server: {
             fs: {

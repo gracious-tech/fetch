e2079eeeb696db2058049cffa90526948bbc486c
shadow-light <noreply@gracious.tech>
2023-06-15 22:49:18 +1000

    Upgrade all modules

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig(({mode}) => {
                         },
                     },
                 ],
-            }),
+            }, {}),
         ],
         resolve: {
             alias: [

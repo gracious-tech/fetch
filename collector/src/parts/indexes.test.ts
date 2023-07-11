60b3db050c0ecc2c7f172cdc6be05a867f6331f6
Codemis <johnathan@pulos.com>
2023-07-11 15:11:23 -0500

    We now display entries in the given path requested.

--- collector/src/parts/indexes.test.ts
+++ collector/src/parts/indexes.test.ts
@@ -1,64 +1,96 @@
-
+import {join} from 'path'
 import {describe, it} from 'vitest'
+import {generate_index_content, update_indexes} from './indexes'
+import { statSync } from 'fs'
 
-import {generate_index_file, update_indexes} from './indexes'
 
-
-describe('generate_index_file', () => {
-
-    const html = generate_index_file('dist/bibles/eng_bsb/')
+describe('generate_index_content', () => {
 
     it("Returns a HTML string", ({expect}) => {
-        expect(html).toMatch(/^<!doctype html>/i)
+        const html = generate_index_content(join('dist', 'bibles', 'eng_bsb'))
+        const actual = html.toLowerCase()
+        expect(actual).toContain('<!doctype html>')
+        expect(actual).toContain('<html>')
+        expect(actual).toContain('</html>')
     })
 
-    it("Contains links to the different formats available", ({expect}) => {
-        expect(html).toMatch(/<a href="usfm\/">.*<a href="usx\/">/)
+    describe("Handling directories", () => {
+
+        const html = generate_index_content(join('dist', 'bibles', 'eng_bsb'))
+        const actual = html.toLowerCase()
+
+        it("Contains links to the different available directories", ({expect}) => {
+            expect(actual).toContain('<a href="./txt/">txt</a>')
+            expect(actual).toContain('<a href="./usfm/">usfm</a>')
+            expect(actual).toContain('<a href="./usx/">usx</a>')
+            expect(actual).toContain('<a href="./html/">html</a>')
+        })
+    
+        it("Contains the number of files in directories", ({expect}) => {
+            expect(actual).toContain('<span data-entry-name="txt">66</span>')
+            expect(actual).toContain('<span data-entry-name="usfm">66</span>')
+            expect(actual).toContain('<span data-entry-name="usx">66</span>')
+            expect(actual).toContain('<span data-entry-name="html">66</span>')
+        })
+
     })
 
-    it("Contains breadcrumb links to parent dirs", ({expect}) => {
-        expect(html).toMatch(/<a.+?>bibles<\/a>/)
-    })
-})
+    describe("Handling files", () => {
 
+        const html = generate_index_content(join('dist', 'bibles'))
+        const actual = html.toLowerCase()
 
-describe('update_indexes', () => {
+        it("Contains links to the different available files", ({expect}) => {
+            expect(actual).toContain('<a href="./manifest.json">manifest.json</a>')
+        })
+    
+        it("Contains the number of files", ({expect}) => {
+            const expected_size = statSync(join('dist', 'bibles', 'manifest.json')).size
+            expect(actual).toContain(`<span data-entry-name="manifest.json">${expected_size.toLocaleString()} bytes</span>`)
+        })
 
-    it("Generates parent dir index when file changes", ({expect}) => {
-        const out = update_indexes(['dist/bibles/manifest.json'], [])
-        expect(out.update.map(i => i.path)).toEqual(['dist/bibles/'])
-        expect(out.remove).toHaveLength(0)
-    })
-
-    it("Doesn't duplicate when files in same dir changed", ({expect}) => {
-        const out = update_indexes(['dist/bibles/manifest.json', 'dist/bibles/fake'], [])
-        expect(out.update).toHaveLength(1)
-        expect(out.remove).toHaveLength(0)
-    })
-
-    it("Generates parent dir indexes when a file removed", ({expect}) => {
-        const out = update_indexes([], ['dist/bibles/fake'])
-        // Parent dir needs to remove file, and grandparent dir needs to update size of parent dir
-        expect(out.update.map(i => i.path).sort()).toEqual(['dist/', 'dist/bibles/'])
-        expect(out.remove).toHaveLength(0)
-    })
-
-    it("Removes parent index when no more files", ({expect}) => {
-        const out = update_indexes([], ['dist/bibles/fake/file'])
-        expect(out.update.map(i => i.path).sort()).toEqual(['dist/', 'dist/bibles/'])
-        expect(out.remove).toEqual(['dist/bibles/fake/'])
-    })
-
-    it("Removes multiple parents when no more files", ({expect}) => {
-        const out = update_indexes([], ['dist/bibles/fake/fake/file'])
-        expect(out.update.map(i => i.path).sort()).toEqual(['dist/', 'dist/bibles/'])
-        expect(out.remove.sort()).toEqual(['dist/bibles/fake/', 'dist/bibles/fake/fake/'])
-    })
-
-    it("Doesn't generate index for a dir higher than `dist/`", ({expect}) => {
-        const out = update_indexes(['dist/file'], [])
-        expect(out.update.map(i => i.path)).toEqual(['dist/'])
-        expect(out.remove).toHaveLength(0)
     })
 
 })
+
+
+// describe('update_indexes', () => {
+
+//     it("Generates parent dir index when file changes", ({expect}) => {
+//         const out = update_indexes(['dist/bibles/manifest.json'], [])
+//         expect(out.update.map(i => i.path)).toEqual(['dist/bibles/'])
+//         expect(out.remove).toHaveLength(0)
+//     })
+
+//     it("Doesn't duplicate when files in same dir changed", ({expect}) => {
+//         const out = update_indexes(['dist/bibles/manifest.json', 'dist/bibles/fake'], [])
+//         expect(out.update).toHaveLength(1)
+//         expect(out.remove).toHaveLength(0)
+//     })
+
+//     it("Generates parent dir indexes when a file removed", ({expect}) => {
+//         const out = update_indexes([], ['dist/bibles/fake'])
+//         // Parent dir needs to remove file, and grandparent dir needs to update size of parent dir
+//         expect(out.update.map(i => i.path).sort()).toEqual(['dist/', 'dist/bibles/'])
+//         expect(out.remove).toHaveLength(0)
+//     })
+
+//     it("Removes parent index when no more files", ({expect}) => {
+//         const out = update_indexes([], ['dist/bibles/fake/file'])
+//         expect(out.update.map(i => i.path).sort()).toEqual(['dist/', 'dist/bibles/'])
+//         expect(out.remove).toEqual(['dist/bibles/fake/'])
+//     })
+
+//     it("Removes multiple parents when no more files", ({expect}) => {
+//         const out = update_indexes([], ['dist/bibles/fake/fake/file'])
+//         expect(out.update.map(i => i.path).sort()).toEqual(['dist/', 'dist/bibles/'])
+//         expect(out.remove.sort()).toEqual(['dist/bibles/fake/', 'dist/bibles/fake/fake/'])
+//     })
+
+//     it("Doesn't generate index for a dir higher than `dist/`", ({expect}) => {
+//         const out = update_indexes(['dist/file'], [])
+//         expect(out.update.map(i => i.path)).toEqual(['dist/'])
+//         expect(out.remove).toHaveLength(0)
+//     })
+
+// })
--- collector/src/parts/indexes.ts
+++ collector/src/parts/indexes.ts
@@ -1,22 +1,86 @@
 
-import {read_dir} from './utils.js'
+import { partition } from 'lodash-es'
+import {DirectoryEntry, get_dir_entries} from './utils.js'
 
 
-export function generate_index_file(path:string):string{
-    // Generate a HTML index file for given directory
+/**
+ * Generate the HTML content using the specific directory path content
+ *
+ * @param path The path to generate an index content for
+ *
+ * @returns The html content
+ */
+export function generate_index_content(path:string): string {
+    // List files and folders separately
+    const sorter = (a: DirectoryEntry, b: DirectoryEntry) => a.name.localeCompare(b.name)
+    const contents = get_dir_entries(path)
+    const results = partition(contents, (content: DirectoryEntry) => content.isDirectory)
+    const dirs = results[0].sort(sorter)
+    const files = results[1].sort(sorter)
+    const max = Math.max(dirs.length, files.length)
+    const rows = []
+    for (let index = 0; index < max; index++) {
+        let folder_text = ''
+        let folder_size = ''
+        let file_text = ''
+        let file_size = ''
+        if (dirs[index] !== undefined) {
+            const name = dirs[index]!.name
+            folder_text = `<a href="./${name}/">${name}</a>`
+            folder_size = `<span data-entry-name="${name}">${dirs[index]!.contentSize}</span>`
+        }
+        if (files[index] !== undefined) {
+            const name = files[index]!.name
+            file_text = `<a href="./${name}">${name}</a>`
+            file_size = `<span data-entry-name="${name}">${files[index]!.fileSize.toLocaleString()} bytes</span>`
+        }
+        rows.push(`<tr>
+            <td>${folder_text}</td>
+            <td>${folder_size}</td>
+            <td>${file_text}</td>
+            <td>${file_size}</td>
+        </tr>`)
+    }
+    const table = `<table class="entry-list">
+        <thead>
+            <tr>
+                <th>Folders</th>
+                <th>Total Files</th>
+                <th>Files</th>
+                <th>Size</th>
+            </tr>
+        </thead>
+        <tbody>${rows.join('')}</tbody>
+    </table>`
     /* TODO Requirements:
-        * List files and folders separately
-        * Display in two columns (folders | files) so can see both even if lists very long
-        * Standard ASCII sorting (.sort() is fine)
-        * Show size of files in bytes (2,000 bytes rather than 2 KB) for easier comparison
-        * Show number of items in folders (will have to readDir for each)
-        * Do as efficiently as possible with least reads (though not overly important)
-        * All items are links
         * Include breadcrumb links at top to step up to each parent, all the way to root
-        * Monospace font, and right-align numbers
-        * Dir paths should always end in a slash
     */
-    return ''
+    return `<!DOCTYPE html>
+        <html>
+            <style>
+                body {
+                    font-family: monospace, monospace;
+                    font-size: 16px;
+                }
+                table, th, td {
+                    border: 1px solid black;
+                }
+                th, td {
+                    padding: .5rem;
+                }
+                table {
+                    border-collapse: collapse;
+                    width: 100%;
+                }
+                td {
+                    width: 25%;
+                }
+                td:nth-child(2), td:nth-child(4) {
+                    text-align: end;
+                }
+            </style>
+            <body>${table}</body>
+        </html>`
 }
 
 
@@ -25,7 +89,7 @@ interface UpdateIndexesReturn {
     remove:string[]
 }
 
-export function update_indexes(modified:string[], removed:string[]):UpdateIndexesReturn{
+export function update_indexes(modified:string[], removed:string[]): UpdateIndexesReturn {
     // Determine what updates are needed for published dir index files
     /* TODO Requirements:
         This function takes a list of files in collection that have been modified or removed.
